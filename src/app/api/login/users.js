3e2cb0a83cd65e9e79fbc78baa9d3c62cbd0dc8c
Md. Kobir Hossain <kobir.hossain@annanovas.com>
2025-05-16 19:08:36 +0600

    Initial Commit

---
+++ src/app/api/login/users.js
@@ -0,0 +1,11 @@
+// ** Fake user data and data type
+// =============== Fake Data ============================
+export const users = [
+  {
+    id: 1,
+    name: 'Admin',
+    password: 'An@123456',
+    email: 'admin@annanovas.com',
+    image: '/images/avatars/1.png'
+  }
+]
