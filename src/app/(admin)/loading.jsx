d9273103a9c3736f847a27e092b2b550346eb4b0
Md. Kobir Hossain <kobir.hossain@annanovas.com>
2025-05-21 13:35:46 +0600

    Added Loading page

---
+++ src/app/(admin)/loading.jsx
@@ -0,0 +1,8 @@
+
+import LoadingProgress from "@views/LoadingProgress";
+
+const Loading = () => {
+  return <LoadingProgress />
+}
+
+export default Loading
---
+++ src/views/LoadingProgress.jsx
@@ -0,0 +1,14 @@
+// MUI Imports
+import LinearProgress from '@mui/material/LinearProgress'
+
+const LoadingProgress = () => {
+  return (
+    <div className="fixed inset-0 z-[9999]">
+      <LinearProgress className="w-full bg-transparent" />
+    </div>
+  )
+
+
+}
+
+export default LoadingProgress
